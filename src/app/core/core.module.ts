b6b0d7fa8bdbd991eb5a808f6e9c3f141d865a5c
Mauricio <maubot01@gmail.com>
2017-05-16 11:09:52 -0400

    Comenzando el proyecto

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShellComponent } from './shell/shell.component';
+import { TopBarComponent } from './Shell/top-bar/top-bar.component';
+import { MainContentComponent } from './Shell/main-content/main-content.component';
+import { Routes, RouterModule } from '@angular/router';
+const routes: Routes = [
+  {
+    path:'', loadChildren:'./../home/home.module#HomeModule'
+  },
+  {
+    path:'*', loadChildren:'./../home/home.module#HomeModule'
+  }
+];
+ 
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [ShellComponent, TopBarComponent, MainContentComponent],
+  exports: [ShellComponent]
+})
+export class CoreModule { }
