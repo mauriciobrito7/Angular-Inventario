df23abf3287dc69f47afcee8384e6b0a727c542c
Mauricio <maubot01@gmail.com>
2017-07-07 17:16:24 -0400

    arreglando problema de calculo de precios y ganancias porque concatenaba el resultado

--- src/app/pipes/calculator-earn.pipe.ts
+++ src/app/pipes/calculator-earn.pipe.ts
@@ -7,13 +7,13 @@ export class CalculatorEarnPipe implements PipeTransform {
 
   transform(value: string, args?: any[]): number {
     let precioDolares = parseFloat(value) + (parseFloat(value)* 0.07);
-    let precioDolarToday = args[0];
-    let prime = args[1];
-    let porcentaje =  0.01 * args[2];
-    let envio = args[3];
+    let precioDolarToday = parseFloat(args[0]);
+    let prime = parseFloat(args[1]);
+    let porcentaje =  0.01 * parseFloat(args[2]);
+    let envio = parseFloat(args[3]);
     let precioTotal = ( precioDolarToday * ( precioDolares + prime)) + envio;
     precioTotal = (precioTotal * porcentaje);
-    return (isNaN(precioTotal) || precioDolares == 0 ? 0 : parseInt(precioTotal));
+    return (isNaN(precioTotal) || precioDolares == 0 ? 0 : precioTotal);
   }
 
 }
--- src/app/pipes/calculator-price.pipe.ts
+++ src/app/pipes/calculator-price.pipe.ts
@@ -7,10 +7,10 @@ export class CalculatorPricePipe implements PipeTransform {
 
   transform(value: string, args?: any[]): number {
     let precioDolares = parseFloat(value) + (parseFloat(value)* 0.07);
-    let precioDolarToday = args[0];
-    let prime = args[1];
-    let porcentaje =  0.01 * args[2];
-    let envio = args[3];
+    let precioDolarToday = parseFloat(args[0]);
+    let prime = parseFloat(args[1]);
+    let porcentaje =  0.01 * parseFloat(args[2]);
+    let envio = parseFloat(args[3]);
     let precioTotal = ( precioDolarToday * ( precioDolares + prime)) + envio;
     precioTotal = (precioTotal * porcentaje) + precioTotal;
     return (isNaN(precioTotal) || precioDolares == 0 ? 0 : precioTotal);
